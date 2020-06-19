583c8231350d9db39955b772b63da21670395dd1
Byron <byronnp@gmail.com>
2020-06-19 11:43:00 -0500

    se agrega campo id

--- src/main/java/ec/gob/mag/scsemillasmicro/domain/Topografia.java
+++ src/main/java/ec/gob/mag/scsemillasmicro/domain/Topografia.java
@@ -1,5 +1,26 @@
 package ec.gob.mag.scsemillasmicro.domain;
 
-public class Topografia {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
+
+@Entity
+@Table(name = "topografia")
+public class Topografia {
+	
+	@Id
+	@ApiModelProperty(value = "Este campo es la clave primaria de la tabla", required = true, readOnly = true)
+	@Column(name = "top_id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("topid")
+	private Long topid;
+	
+	
 }
